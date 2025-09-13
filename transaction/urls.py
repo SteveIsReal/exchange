244cf6d294835e30ec185d6cc12ce34931a36588
Pyn Chaingin <stevie@Pyns-Macbook-Pro.local>
2025-09-13 10:41:18 +0700

    second commit

--- transaction/urls.py
+++ transaction/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from transaction import views
 
 urlpatterns = [
-    path('exchange/', views.exchange.as_view())
+    path('exchange/', views.Exchange.as_view()),
+    path('login/', views.Login.as_view())
 
 ]
--- transaction/views.py
+++ transaction/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.views import APIView
+from rest_framework import generics
 from transaction.models import Transaction
+from transaction.serial import TransactionSerializer
 import json
 
-class exchange(APIView):
+class Exchange(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class Login(APIView):
     def get(self, request):
-        return render(request, 'index.html', None)
+        return render(request, 'login_page.html', None)
     def post(self, request):
-        return HttpResponse('Post')
-    def patch(self, request):
-        return HttpResponse('Patch')
-    def delete(self, request):
-        return HttpResponse('Del')
+        return render(request, 'index.html', None)
+
+
+
