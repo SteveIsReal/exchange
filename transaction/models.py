2210826100266733898fdcb3b7242189fc26d161
Pyn Chaingin <stevie@Pyns-Macbook-Pro.local>
2025-09-15 09:21:06 +0700

    image

--- transaction/models.py
+++ transaction/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-
 class Transaction(models.Model):
     type = models.CharField(max_length=6, choices=(('input',"INPUT"), ('output', "OUTPUT")))
     amount = models.IntegerField()
     note = models.CharField(max_length=100, null=True, blank=True)
     action_datetime = models.DateTimeField()
-
  
